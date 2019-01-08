4d84fac89180c51e242a4490985c7d9bcfdabd00
fed0rus <anarsis007@gmail.com>
2019-01-08 19:52:30 +0700

    fucked up.

--- sandbox-RUS/sokolUserBalance[RUS]1task4pack.py
+++ sandbox-RUS/sokolUserBalance[RUS]1task4pack.py
@@ -1,21 +1,26 @@
-import web3
-from web3.auto import w3
+from web3 import Web3
 
 class User(object):
     def __init__(self, UUID, PIN):
         self.UUID = str(UUID)
         self.PIN = str(PIN)
 
-    def extractPin(self):
+    def extractPIN(self):
         return [int(k) for k in self.PIN]
 
     def extractUUID(self):
         return (self.UUID).replace('-', '')
 
     def generatePrivateKey(self): # UUID <- str ... PIN <- [int]
-        UUID = self.extractedUUID()
+        UUID = self.extractUUID()
         PIN = self.extractPIN()
-        privateKey = web3.utils.soliditySha3('')
+        privateKey = Web3.soliditySha3(["bytes16"], [b''])
         for k in range(4):
-            privateKey = web3.utils.soliditySha3(privateKey, UUID, PIN[k]) # add abi.encodePacked()
+            privateKey = Web3.soliditySha3(["bytes16", "bytes16", "int8[]"], [privateKey, bytearray.fromhex(UUID), PIN[k]]) # add abi.encodePacked()
         self.privateKey = privateKey
+
+uuid = input()
+pin = input()
+user = User(uuid, pin)
+user.generatePrivateKey()
+print(user.privateKey)
