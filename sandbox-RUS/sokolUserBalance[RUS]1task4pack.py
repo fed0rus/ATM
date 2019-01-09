3fcfffd2fea6f1cd41d6c632926477e71af03a8e
fed0rus <anarsis007@gmail.com>
2019-01-09 14:37:29 +0700

    .

--- sandbox-RUS/sokolUserBalance[RUS]1task4pack.py
+++ sandbox-RUS/sokolUserBalance[RUS]1task4pack.py
@@ -1,4 +1,11 @@
-from web3 import Web3
+# Stages:
+# 0 [DONE]   Configure the host [SKL]
+# 1 [DONE]   Generate privateKey
+# 2 [INPR]   Generate address
+# 3 [AWAT]   Send a request to the Sokol server and receive the fucking account!
+
+from web3 import Web3, HTTPProvider
+from eth_account import Account
 
 class User(object):
     def __init__(self, UUID, PIN):
@@ -14,13 +21,33 @@ class User(object):
     def generatePrivateKey(self):
         UUID = self.extractUUID()
         PIN = self.extractPIN()
-        privateKey = Web3.soliditySha3(["bytes16"], [b''])
+        privateKey = Web3.soliditySha3(["bytes32"], [b''])
         for k in range(4):
-            privateKey = Web3.soliditySha3(["bytes16", "bytes16", "int8[]"], [privateKey, bytearray.fromhex(UUID), [PIN[k]]]) # add abi.encodePacked()
+            privateKey = Web3.soliditySha3(["bytes32", "bytes16", "int8[]"], [privateKey, bytearray.fromhex(UUID), [PIN[k]]]) # ABI-packed, keccak256 hashed
         self.privateKey = privateKey
 
-uuid = input()
-pin = input()
-user = User(uuid, pin)
+    def generateAddress(self):
+        account = Account.privateKeyToAccount(self.privateKey)
+        self.address = account.address
+
+# Stage 0
+
+server = Web3(HTTPProvider("https://sokol.poa.network/"))
+
+# Stage 1
+
+UUID = input()
+PIN = input()
+user = User(UUID, PIN)
 user.generatePrivateKey()
-print(user.privateKey)
+
+# Stage 2
+
+user.generateAddress()
+print(user.address)
+print((Web3.soliditySha3(["bytes32"], ["Ethereum is a distributed database".encode("utf-8")])).hex())
+
+'''
+a52b5033-35d1-4aa6-8190-72f0116edba3
+1741
+'''
