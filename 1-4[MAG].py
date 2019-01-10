cb05501f4cfd4ee1c9b53f567a97474a73d2f24e
FluFFka <45143652+FluFFka@users.noreply.github.com>
2019-01-10 18:55:31 +0700

    Update 1-4[MAG].py

--- 1-4[MAG].py
+++ 1-4[MAG].py
@@ -1,44 +1,47 @@
-data = eval(input())
-xeloE = 252.
-yeloE = 331.
-xntE = 520.
-yntE = 634.
-xeroE = 782.
-yeroE = 321.
-for i in range(len(data)):
-    dx = data[i]['faceRectangle']['left']
-    dy = data[i]['faceRectangle']['top']
-    kx = 1000 / (data[i]['faceRectangle']['width'])
-    ky = 1000 / (data[i]['faceRectangle']['height'])
-    for j, k in data[i]['faceLandmarks'].items():
-        data[i]['faceLandmarks'][j]['x'] = kx * (data[i]['faceLandmarks'][j]['x'] - dx)
-        data[i]['faceLandmarks'][j]['y'] = ky * (data[i]['faceLandmarks'][j]['y'] - dy)
-        if (j == 'eyeLeftOuter'):
-            xelo = k['x']
-            yelo = k['y']
-        if (j == 'eyeRightOuter'):
-            xero = k['x']
-            yero = k['y']
-        if (j == 'noseTip'):
-            xnt = k['x']
-            ynt = k['y']
-    a2 = ((xeloE - xntE) * (xnt - xero) - (xntE - xeroE) * (xelo - xnt)) / ((xnt - xero) * (yelo - ynt) - (xelo - xnt) * (ynt - yero))
-    a1 = ((xeloE - xntE) - a2 * (yelo - ynt)) / (xelo - xnt)
-    a0 = xeloE - xelo * a1 - yelo * a2
-    b2 = ((yeloE - yntE) * (xnt - xero) - (yntE - yeroE) * (xelo - xnt)) / ((xnt - xero) * (yelo - ynt) - (xelo - xnt) * (ynt - yero))
-    b1 = ((yeloE - yntE) - b2 * (yelo - ynt)) / (xelo - xnt)
-    b0 = yeloE - xelo * b1 - yelo * b2
-    for j, k in data[i]['faceLandmarks'].items():
-        xs = data[i]['faceLandmarks'][j]['x']
-        ys = data[i]['faceLandmarks'][j]['y']
-        x, y = xs, ys
-        xs = a0 + x*a1 + y*a2
-        ys = b0 + x*b1 + y*b2
-        data[i]['faceLandmarks'][j]['x'] = xs
-        data[i]['faceLandmarks'][j]['y'] = ys
-delta = [0, 0, 0]
-for i in range(3):
-    for j, k in data[i]['faceLandmarks'].items():
-        delta[i] += (data[i]['faceLandmarks'][j]['x'] - data[3]['faceLandmarks'][j]['x'])**2 + (data[i]['faceLandmarks'][j]['y'] - data[3]['faceLandmarks'][j]['y'])**2
-print(delta.index(min(delta)))
+faces = eval(input())
+fl = 'faceLandmarks'
+fr = 'faceRectangle'
 
+for i in range(len(faces)):
+    dx, dy = faces[i][fl]['noseTip']['x'], faces[i][fl]['noseTip']['y']
+    for j, k in faces[i][fl].items():
+        faces[i][fl][j]['x'] -= dx
+        faces[i][fl][j]['y'] -= dy
+
+    xs, ys = faces[i][fl]['eyeLeftOuter']['x'], faces[i][fl]['eyeLeftOuter']['y']
+    xe, ye = 0, (-1) * (xs**2 + ys**2)**0.5
+    sina = (xs * ye - xe * ys) / (2 * ye**2)
+    cosa = (xs * xe + ys * ye) / (2 * ye**2)
+    for j, k in faces[i][fl].items():
+        xc, yc = faces[i][fl][j]['x'], faces[i][fl][j]['y']
+        faces[i][fl][j]['x'] = xc * cosa - yc * sina
+        faces[i][fl][j]['y'] = xc * sina + yc * cosa
+
+for i in range(len(faces)):
+    dx, dy = 10000, 10000
+    maxx, maxy = -10000, -10000
+    for j, k in faces[i][fl].items():
+        maxx = max(maxx, faces[i][fl][j]['x'])
+        dx = min(dx, faces[i][fl][j]['x'])
+        maxy = max(maxy, faces[i][fl][j]['y'])
+        dy = min(dy, faces[i][fl][j]['y'])
+    maxx -= dx
+    maxy -= dy
+    for j, k in faces[i][fl].items():
+        faces[i][fl][j]['x'] -= dx
+        faces[i][fl][j]['y'] -= dy
+    coeffx = 1000 / maxx
+    coeffy = 1000 / maxy
+    for j, k in faces[i][fl].items():
+        faces[i][fl][j]['x'] *= coeffx
+        faces[i][fl][j]['y'] *= coeffy
+
+
+
+delta = [0, 0, 0]
+for i in range(len(faces) - 1):
+    for j, k in faces[i][fl].items():
+        x0, y0 = faces[i][fl][j]['x'], faces[i][fl][j]['y']
+        x1, y1 = faces[3][fl][j]['x'], faces[3][fl][j]['y']
+        delta[i] += ((x1 - x0)**2 + (y1 - y0)**2)**0.5
+print(delta.index(min(delta)))
