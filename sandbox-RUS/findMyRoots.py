f79caf1113cb941a68106372c12307f29beaec70
Ruslan Fedorov <fed0rus@users.noreply.github.com>
2019-01-11 17:11:24 +0700

    .

--- sandbox-RUS/findMyRoots.py
+++ sandbox-RUS/findMyRoots.py
@@ -2,24 +2,9 @@ import web3
 from web3 import Web3, HTTPProvider
 server = Web3(HTTPProvider("https://sokol.poa.network/"))
 
-# Clean tx's receipt
+allRootBlocks = [6071558, 6070919, 6071689, 6071818, 6071307, 6071058, 6071188, 6071576, 6070939, 6071837, 6071457, 6071333, 6071081, 6070959, 6071731, 6071098, 6071227, 6071356, 6070845, 6071484, 6071617, 6071750, 6071111, 6070864, 6070993, 6071248, 6071505, 6071386, 6071136, 6071779, 6070884, 6071270, 6071526, 6071016, 6071284, 6070901, 6071669, 6071796, 6071161, 6071419]
 
-def AttributeDict(self):
-    return self
-
-def HexBytes(self):
-    return self
-
-def filteredTX(tx):
-    return eval(str(tx)[14:-1])
-
-class User(object):
-    def __init__(self, address):
-        self.address = address
-        self.rootTokens = []
-
-
-user = User(input("Your address: "))
+userAddress = input("Your address: ")
 file = open("eventLogs.txt", 'r')
 eventLogs = ''
 for i in file:
@@ -27,72 +12,33 @@ for i in file:
 eventLogs = eval(eventLogs)
 file.close()
 
-parent = user.address
 used = {} # key is address,
 global blocks
 blocks = []
 
-trans = dict()
-n = 0
-for event in eventLogs:
-    if event['args']['tx_source'] == b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00':
-        continue
-    else:
-        if trans.get(event['args']['recipient']) == None:
-            trans[event['args']['recipient']] = [filteredTX(server.eth.getTransaction(event['transactionHash']))['from']]
-            print(n)
-            n += 1
-        else:
-            trans[event['args']['recipient']].append(filteredTX(server.eth.getTransaction(event['transactionHash']))['from'])
-            print(n)
-            n += 1
-utxoPool = open("pool.txt", "w+")
-utxoPool.write(str(trans))
-utxoPool.close()
-
-
-
+answer = []
+poolFile = open("pool.txt", 'r')
+trans = eval(poolFile.read())
+poolFile.close()
 i = 0
-print('112212')
 def dfs(used, current): # current = node address
     global i
     used[current] = 1
     senders = []
     print('in')
-    o = 0
-    '''
-    for event in eventLogs:
-        if event['args']['tx_source'] == b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00':
-            BN = filteredTX(server.eth.getTransaction(event['transactionHash']))['blockNumber']
-            blocks.append(BN)
-        if event['args']['recipient'] == current:
-            senders = trans[current]
-            '''
+    f = 0
     for sender in trans[current]:
         f = 1
         if used.get(sender) == None:
             i += 1
             print(i)
             dfs(used, sender)
-dfs(used, parent)
-print(set(blocks))
+    if f == 0:
+        for event in eventLogs:
+            if event['args']['tx_source'] == b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00':
+                if event['args']['recipient'] == current:
+                    tx = filteredTX(server.eth.getTransaction(event['transactionHash']))
+                    answer.append(tx['blockNumber'])
 
-'''
-EL example
-{
-    'args': {
-        'tx_source': b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
-        'tx_address': b"\x93\xcdp\xa0f'\xb14+\x9f\xc3\x97\xa0\x8e\xe6\x9b\xe9\x16L\x91\x06\xcc\x1cG~\xbb\xb2#P\x0cii",
-        'recipient': '0x2eD61BCA22E097506FCd9Eb1A0C5256a1f974604',
-        'value': 3617920,
-        'vout': 0
-    },
-    'event': 'Transfer',
-    'logIndex': 0,
-    'transactionIndex': 1,
-    'transactionHash': '0x75be2017e3c874d1a517d90f7aa5ecab0867e962e9bfda1412aac00f6d1250d4',
-    'address': '0xe87A3686B0A42d66EEe76D48c9A8307c27D14D1c',
-    'blockHash': '0x4eab261d16e9221b3e482c8d52268e51a816cbf69f23e9d4e0a23fcc3ac15ddb',
-    'blockNumber': 6070845
-}
-'''
+dfs(used, userAddress)
+print(answer)
