c472d869728a84a803cac8c022a45aa38fd11e8a
Ruslan Fedorov <fed0rus@users.noreply.github.com>
2019-02-04 21:55:51 +0700

    .

--- ETC/contract/registrar[RUS].py
+++ ETC/contract/registrar[RUS].py
@@ -22,54 +22,14 @@ def generateContract():
     privateKey = extractPrivateKey()
     contractOwnerAddress = generateAddressFromPrivateKey(privateKey)
     source = '''
-    pragma solidity ^5.0.1;
-
-    contract Mortal {
-        address owner;
-
-        constructor() public {
-            require(%s != address(0));
-            owner = %s;
-        }
-
-        modifier ownerOnly {
-            require(msg.sender == owner);
-            _;
-        }
-    }
-
-    contract KYC is mortal {
-
-        event Register(indexed address customerAddress, indexed string customerName)
-        mapping (address => string) public addressToCustomerName;
-        mapping (string => address) public customerNameToAddress
-
-        function registerCustomer(string customerName) public {
-            require(name != '', "Please, enter valid name");
-            require(msg.sender != address(0));
-            require(addressToCustomerName[msg.sender] == false, "You are already registered")
-            require(customerNameToAddress[customerName] == false)
-            addressToCustomerName[msg.sender] = customerName;
-            customerNameToAddress[customerName] = msg.sender;
-            emit Register(msg.sender, customerName);
-        }
-
-        function deleteCustomer(address customerAddressToDelete) public {
-            require(msg.sender == customerAddressToDelete);
-            // Delete from DBs
-        }
-
-        // Add some mortality
-        function deleteContract() public ownerOnly {
-            selfdestruct(owner);
-        }
-    }
     ''' % (contractOwnerAddress, contractOwnerAddress)
     compiledSource = compile_source(source)
     contractInterface = compiledSource["<stdin>:KYC"]
     Contract = server.eth.contract(abi=contractInterface['abi'], bytecode=contractInterface['bin'])
-    print("contractInterface:")
-    print(contractInterface)
+    pass
+def main():
+    server = Web3(HTTPProvider("https://sokol.poa.network"))
+    generateContract()
 
-server = Web3(HTTPProvider("https://sokol.poa.network"))
-generateContract()
+if __name__ == "__main__":
+    main()
--- sandbox-RUS/4-2[RUS]/getLogs.py
+++ sandbox-RUS/4-2[RUS]/getLogs.py
@@ -11,12 +11,11 @@ def HexBytes(self):
 
 server = Web3(HTTPProvider("https://sokol.poa.network/"))
 
-contractAddress = server.toChecksumAddress('0xe87a3686b0a42d66eee76d48c9a8307c27d14d1c')
-contractABI = [{"type":"function","stateMutability":"nonpayable","payable":False,"outputs":[],"name":"withdraw","inputs":[{"type":"uint256","name":"_value"}],"constant":False},{"type":"function","stateMutability":"nonpayable","payable":False,"outputs":[],"name":"transfer","inputs":[{"type":"bytes32","name":"_txHash"},{"type":"uint256","name":"_vout"},{"type":"address[]","name":"_recipients"},{"type":"uint256[]","name":"_values"}],"constant":False},{"type":"constructor","stateMutability":"nonpayable","payable":False,"inputs":[]},{"type":"fallback","stateMutability":"payable","payable":True},{"type":"event","name":"Transfer","inputs":[{"type":"bytes32","name":"tx_source","indexed":True},{"type":"bytes32","name":"tx_address","indexed":True},{"type":"address","name":"recipient","indexed":True},{"type":"uint256","name":"value","indexed":False},{"type":"uint256","name":"vout","indexed":False}],"anonymous":False}]
-
+contractAddress = server.toChecksumAddress("0x119041553c1d9f7d0D6Af5B7A0954B2591d8C2dC")
+contractABI = [{"type":"function","stateMutability":"nonpayable","payable":False,"outputs":[],"name":"deleteContract","inputs":[],"constant":False},{"type":"function","stateMutability":"nonpayable","payable":False,"outputs":[],"name":"gSS","inputs":[],"constant":False},{"type":"event","name":"AddName","inputs":[{"type":"string","name":"name","indexed":False}],"anonymous":False}]
 contract = server.eth.contract(address=contractAddress, abi=contractABI)
 
-eventFilter = contract.events.Transfer.createFilter(fromBlock=0, toBlock='latest')
+eventFilter = contract.events.AddName.createFilter(fromBlock=0, toBlock='latest')
 log = eventFilter.get_all_entries()
 
 for index in range(len(log)):
@@ -28,23 +27,3 @@ for event in log:
     eventLog.write(str(event) + ",\n")
 
 eventLog.close()
-
-'''
-returns:
-{
-    'args': {
-            'tx_source': b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
-            'tx_address': b"\x93\xcdp\xa0f'\xb14+\x9f\xc3\x97\xa0\x8e\xe6\x9b\xe9\x16L\x91\x06\xcc\x1cG~\xbb\xb2#P\x0cii",
-            'recipient': '0x2eD61BCA22E097506FCd9Eb1A0C5256a1f974604',
-            'value': 3617920,
-            'vout': 0
-    },
-    'event': 'Transfer',
-    'logIndex': 0,
-    'transactionIndex': 1,
-    'transactionHash': '0x75be2017e3c874d1a517d90f7aa5ecab0867e962e9bfda1412aac00f6d1250d4',
-    'address': '0xe87A3686B0A42d66EEe76D48c9A8307c27D14D1c',
-    'blockHash': '0x4eab261d16e9221b3e482c8d52268e51a816cbf69f23e9d4e0a23fcc3ac15ddb',
-    'blockNumber': 6070845
-}
-'''
