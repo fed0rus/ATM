ce45a36b5aab5ea789110c18a43693fd13531652
fed0rus <anarsis007@gmail.com>
2019-03-07 15:25:41 +0300

    Refinements after the first iterations.

---
+++ finals/face-management.py
@@ -0,0 +1,368 @@
+#!/usr/bin/env python
+import cv2
+import requests
+import argparse
+import numpy as np
+import os
+import dlib
+import web3
+from random import randrange
+
+
+def SetArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--simple-add',
+        type=str,
+    )
+    parser.add_argument(
+        '--train',
+        action='store_true',
+    )
+    parser.add_argument(
+        '--list',
+        action='store_true',
+    )
+    parser.add_argument(
+        '--del',
+        type=str,
+    )
+    parser.add_argument(
+        '--find',
+        type=str,
+    )
+
+    args = parser.parse_args()
+    return vars(args)
+
+
+def GetKey():
+    with open('faceapi.json') as f:
+        privateKey = eval(f.read())['key']
+    return privateKey
+
+def GetGroupId():
+    with open('faceapi.json') as f:
+        groupId = eval(f.read())['groupId']
+    return groupId
+
+def GetBaseUrl():
+    with open('faceapi.json') as f:
+        serviceUrl = eval(f.read())['serviceUrl']
+    return serviceUrl
+
+
+def GetList():
+    headers = {
+        'Ocp-Apim-Subscription-Key': GetKey(),
+    }
+    baseUrl = GetBaseUrl() + 'persongroups/' + GetGroupId() + '/persons'
+    req = requests.get(
+        baseUrl,
+        headers=headers,
+    )
+    return req
+
+def GetPersons():
+    req = GetList().json()
+    persons = dict()
+    for person in req:
+        persons[person['name']] = person['personId']
+    return persons
+
+def CreateGroup():
+    headers = {
+        'Content-Type' : 'application/json',
+        'Ocp-Apim-Subscription-Key': GetKey(),
+    }
+    params = {
+        'personGroupId' : GetGroupId(),
+    }
+    data = {
+        'name' : GetGroupId(),
+    }
+    baseUrl = GetBaseUrl() + 'persongroups/' + GetGroupId()
+    req = requests.put(
+        baseUrl,
+        params=params,
+        headers=headers,
+        json=data,
+    )
+    if (str(req) == '<Response [200]>'):
+        return 'Success'
+    else:
+        return req.json()
+
+def GetVideoFrames(videoName):
+    vcap = cv2.VideoCapture(videoName)
+    result = []
+    frames = []
+    while (True):
+        ret, frame = vcap.read()
+        if (frame is None):
+            break
+        else:
+            frames.append(frame)
+    if (len(frames) < 5):
+        return result
+    for i in range(0, len(frames), len(frames) // 4):
+        if (len(result) == 4):
+            break
+        result.append(frames[i])
+    result.append(frames[-1])
+    vcap.release()
+    return result
+
+def GetOctetStream(image):
+    ret, buf = cv2.imencode('.jpg', image)
+    return buf.tobytes()
+
+def MakeDetectRequest(buf):
+    headers = {
+        'Content-Type': 'application/octet-stream',
+        'Ocp-Apim-Subscription-Key': GetKey(),
+    }
+    params = {
+        'returnFaceId':True,
+        'returnFaceRectangle':False,
+    }
+    baseUrl = GetBaseUrl() + 'detect/'
+    req = requests.post(
+        baseUrl,
+        params=params,
+        headers=headers,
+        data=buf,
+    )
+    return req.json()
+
+def Detect(videoFrames):
+    result = []
+    for frame in videoFrames:
+        image = GetOctetStream(frame)
+        req = MakeDetectRequest(image)
+        if (len(req) != 0):
+            result.append(req[0]['faceId'])
+    return result
+
+def CreateFace(name):
+    headers = {
+        'Ocp-Apim-Subscription-Key': GetKey(),
+    }
+    params = {
+        'personGroupId': GetGroupId(),
+    }
+    data = {
+        'name':name,
+    }
+    baseUrl = GetBaseUrl() + 'persongroups/' + GetGroupId() + '/persons'
+    req = requests.post(
+        baseUrl,
+        params=params,
+        headers=headers,
+        json=data,
+    )
+    return req.json()
+
+def MakeAddRequest(image, personId):
+    headers = {
+        'Ocp-Apim-Subscription-Key': GetKey(),
+        'Content-Type' : 'application/octet-stream',
+    }
+    params = {
+        'personGroupId' : GetGroupId(),
+        'personId' : personId,
+    }
+    baseUrl = GetBaseUrl() + 'persongroups/' + GetGroupId() + '/persons/' + personId + '/persistedFaces'
+    req = requests.post(
+        baseUrl,
+        params=params,
+        headers=headers,
+        data = image,
+    )
+    return req.json()
+
+def AddFaces(videoFrames, personId):
+    result = []
+    for frame in videoFrames:
+        image = GetOctetStream(frame)
+        currId = MakeAddRequest(image, personId)
+        result.append(currId['persistedFaceId'])
+    return result
+
+
+def SimpleAdd(videoName):
+    videoFrames = GetVideoFrames(videoName)
+    ids = Detect(videoFrames)  ##IDS OF VIDEOGUY'S FACE
+    if (len(ids) < 3):
+        print('Video does not contain any face')
+    else:
+        CreateGroup()
+        currId = CreateFace(str(randrange(10000000000, 100000000000)))['personId']
+        ids = AddFaces(videoFrames, currId)
+        print('5 frames extracted')
+        print('PersonId: ' + currId)
+        print('FaceIds')
+        print('=======')
+        for id in ids:
+            print(id)
+
+
+def MakeTrainRequest():
+    headers = {
+        'Ocp-Apim-Subscription-Key': GetKey(),
+    }
+    params = {
+        'personGroupId': GetGroupId(),
+    }
+    baseUrl = GetBaseUrl() + 'persongroups/' + GetGroupId() + '/train'
+    req = requests.post(
+        baseUrl,
+        params=params,
+        headers=headers,
+    )
+    if (str(req) == '<Response [202]>'):
+        return 'Success'
+    else:
+        return req.json()
+
+def Train():
+    req = MakeTrainRequest()
+    if (req == 'Success'):
+        persons = GetPersons()
+        coun = 0
+        for i, j in persons.items():
+            coun += 1
+        print('Training task for ' + str(coun) + ' persons started')
+    else:
+        print('Something wrong')
+        print(req)
+
+
+def List():
+    persons = GetPersons()
+    for i, j in persons.items():
+        print("Person's name is " + '"' + str(i) + '"' + "; person's id is " + '"' + j + '"')
+
+
+def MakeDeletePersonRequest(personId):
+    headers = {
+        'Ocp-Apim-Subscription-Key': GetKey(),
+    }
+    params = {
+        'personGroupId': GetGroupId(),
+        'personId': personId,
+    }
+    baseUrl = GetBaseUrl() + 'persongroups/' + GetGroupId() + '/persons/' + personId
+    req = requests.delete(
+        baseUrl,
+        params=params,
+        headers=headers,
+    )
+    return req
+
+def DeletePerson(personId):
+    req = MakeDeletePersonRequest(personId)
+    print(str(req))
+    if (str(req) == '<Response [200]>'):
+        print('Person with id ' + '"' + personId + '"' + ' deleted')
+    else:
+        print('Something wrong')
+
+
+def GetTrainingStatus():
+    headers = {
+        'Ocp-Apim-Subscription-Key': GetKey(),
+    }
+    params = {
+        'personGroupId': GetGroupId(),
+    }
+    baseUrl = GetBaseUrl() + 'persongroups/' + GetGroupId() + '/training'
+    req = requests.get(
+        baseUrl,
+        params=params,
+        headers=headers,
+    )
+    return req.json()
+
+def Identify(videoFrames):
+    headers = {
+        'Ocp-Apim-Subscription-Key': GetKey(),
+    }
+    ids = Detect(videoFrames)
+    data = {
+        'faceIds' : ids,
+        'personGroupId' : GetGroupId(),
+    }
+    baseUrl = GetBaseUrl() + '/identify'
+    req = requests.post(
+        baseUrl,
+        headers=headers,
+        json=data,
+    )
+    return req.json()
+
+def Find(videoName):
+    req = GetTrainingStatus()
+    if (req.get('status') != None):
+        if (req['status'] == 'succeeded'):
+            videoFrames = GetVideoFrames(videoName)
+            if (len(videoFrames) < 5):
+                print('The person cannot be identified')
+            else:
+                result = Identify(videoFrames)
+                candidates = dict()
+                for id in result:
+                    if (id != 'error'):
+                        for candidate in id['candidates']:
+                            if (candidates.get(candidate['personId']) == None):
+                                candidates[candidate['personId']] = candidate['confidence']
+                            else:
+                                candidates[candidate['personId']] += candidate['confidence']
+                    else:
+                        candidates = dict()
+                        break
+                if (len(candidates) == 0):
+                    print('The person cannot be identified')
+                else:
+                    maxValue = 0
+                    bestCandidate = ''
+                    for i, j in candidates.items():
+                        if (j > maxValue):
+                            maxValue = j
+                            bestCandidate = i
+                    if (maxValue >= 2.5):
+                        f = open('person.json', 'w')
+                        ans = '{"id": "' + bestCandidate + '"}'
+                        f.write(ans)
+                        f.close()
+                        print("The person's id is " + '"' + bestCandidate + '"')
+                    else:
+                        print('The person cannot be identified')
+
+
+
+        else:
+            print('The system is not ready yet')
+    else:
+        print('The system is not ready yet')
+
+
+
+def main():
+    args = SetArgs()
+
+    if (args['simple_add'] != None):
+        SimpleAdd(args['simple_add'])
+    if (args['train'] == True):
+        Train()
+    if (args['list'] == True):
+        List()
+    if (args['del'] != None):
+        DeletePerson(args['del'])
+    if (args['find'] != None):
+        Find(args['find'])
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ finals/faceid.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+from eth_abi import encode_abi
+import json
+import requests
+from web3 import Web3, HTTPProvider
+import argparse
+from eth_account import Account
+
+class User(object):
+
+    def __init__(self, UUID, PIN):
+        self.UUID = "0x" + str(UUID.replace('-', ''))
+        self.PIN = str(PIN)
+
+    def setServer(self, server):
+        self.server = server
+
+    def extractPIN(self):
+        return [int(k) for k in self.PIN]
+
+    def generatePrivateKey(self):
+        UUID = self.UUID
+        PIN = self.extractPIN()
+        privateKey = server.solidityKeccak(["bytes16"], [b''])
+        for k in range(4):
+            privateKey = Web3.solidityKeccak(["bytes16", "bytes16", "int8"], [privateKey, UUID, PIN[k]]) # ABI-packed, keccak256 hashed
+        self.privateKey = privateKey
+
+    def generateAddress(self):
+        account = Account.privateKeyToAccount(self.privateKey)
+        self.address = account.address
+
+def scaleValue(value):
+    p = ['wei', 'kwei', 'mwei', 'gwei', 'szabo', 'finney', 'poa']
+    nowp = 0
+    while (value >= 1000 and nowp < 6):
+        value /= 1000
+        nowp += 1
+    return str(round(value, 6)) + p[nowp]
+
+def setArgs():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--balance", action="store", help="Get the balance of your account")
+    args = parser.parse_args()
+    return vars(args)
+
+def getBalanceByID(server):
+    try:
+        with open("person.json", 'r') as person:
+            data = json.load(person)
+            id = str(data["id"])
+        PIN = args["balance"]
+        user = User(id, PIN)
+        user.setServer(server)
+        user.generatePrivateKey()
+        user.generateAddress()
+        balance = scaleValue(server.eth.getBalance(user.address))
+        print("Your balance is {}".format(balance))
+    except:
+        print("ID is not found")
+
+if __name__ == "__main__":
+
+    args = setArgs()
+
+    if args["balance"] is not None:
+        server = Web3(HTTPProvider("https://sokol.poa.network"))
+        getBalanceByID(server)
